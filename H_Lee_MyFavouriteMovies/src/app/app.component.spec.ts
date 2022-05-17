5a3fd2df35cd52f849aca2ea483ec930440eeb8f
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-05-17 00:46:07 -0400

    Created our first angular application and updated README.md

---
+++ H_Lee_MyFavouriteMovies/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'H_Lee_MyFavouriteMovies'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('H_Lee_MyFavouriteMovies');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('H_Lee_MyFavouriteMovies app is running!');
+  });
+});
