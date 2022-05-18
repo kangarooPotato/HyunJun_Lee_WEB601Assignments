d07e4c4937eb6e3a2831158eb15d0554da8dbced
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-05-17 23:04:22 -0400

    Added 1 movie for test

--- H_Lee_MyFavouriteMovies/src/app/app.component.ts
+++ H_Lee_MyFavouriteMovies/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'H_Lee_MyFavouriteMovies';
+
+  movie1: any;
+  movie2: any;
+
+  constructor() {
+    this.movie1 = {
+      id: 0,
+      title: "The Shawshank Redemption",
+      body: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+      author: "Frank Darabont",
+      imageLink: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg",
+      type: "Drama",
+      hashtags: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
+    };
+
+    this.movie2 = {
+      id: 1,
+      title: "title text2",
+      body: "body text2",
+      author: "author text2",
+      imageLink: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg",
+      type: "type text2",
+      hashtags: ["hashthag11","hash22", "hash33"]
+    };
+
+  }
 }
