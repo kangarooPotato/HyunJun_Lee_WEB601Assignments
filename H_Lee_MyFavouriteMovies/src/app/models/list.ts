9ad413e1dd41b2be7d4ef347ea201348bf5fc5ca
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-05-17 22:46:09 -0400

    Temporary Commit for list.ts

---
+++ H_Lee_MyFavouriteMovies/src/app/models/list.ts
@@ -0,0 +1,32 @@
+import {Content} from "./content";
+
+export class List {
+
+  private _items: Content[];
+  constructor(item: Content) {
+    this._items = []; // initialize Content array to be empty
+  }
+
+  get items():Content[] {
+    return this._items;
+  }
+
+  addContent(Content: any) {
+    this._items.push(Content);
+  }
+
+  contentLength(){
+    return this._items.length;
+  }
+
+  readContent(item: number){
+    if(item <= this.contentLength()){
+        return `<h3 style="color: coral">title</h3>
+                <p>something</p>
+                <a>image</a>`;
+    } else {
+      return "<h3>fail</h3>";
+    }
+  }
+
+}
