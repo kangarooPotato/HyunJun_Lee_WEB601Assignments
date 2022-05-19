07d0df67f193cf5b85df5e12727063e616d0b510
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-05-19 15:42:59 -0400

    works

--- H_Lee_MyFavouriteMovies/src/app/app.component.ts
+++ H_Lee_MyFavouriteMovies/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Content } from "./models/content";
+import { List } from "./models/list";
 
 @Component({
   selector: 'app-root',
@@ -7,16 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'H_Lee_MyFavouriteMovies';
-
-  movie1: any;
-  movie2: any;
-  movie3: any;
-  movie4: any;
-
-
+  myList: List;
 
   constructor() {
-    this.movie1 = {
+    let contentitem: Content;
+    this.myList = new List;
+
+    contentitem = {
       id: 0,
       title: "The Shawshank Redemption",
       body: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
@@ -25,8 +24,9 @@ export class AppComponent {
       type: "Drama",
       hashtags: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
     };
+    this.myList.addContent(contentitem);
 
-    this.movie2 = {
+    contentitem = {
       id: 1,
       title: "The Godfather",
       body: "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.",
@@ -35,8 +35,9 @@ export class AppComponent {
       type: "Crime",
       hashtags: ["Marlon Brando","Al Pacino", "James Caan"]
     };
+    this.myList.addContent(contentitem);
 
-    this.movie3 = {
+    contentitem = {
       id: 2,
       title: "The Dark Knight",
       body: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
@@ -45,8 +46,9 @@ export class AppComponent {
       type: "Action",
       hashtags: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"]
     };
+    this.myList.addContent(contentitem);
 
-    this.movie4 = {
+    contentitem = {
       id: 3,
       title: "Inception",
       body: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
@@ -55,6 +57,7 @@ export class AppComponent {
       type: "Sci-Fi",
       hashtags: ["Leonardo DiCaprio","Joseph Gordon","LevittElliot Page"]
     };
+    this.myList.addContent(contentitem);
 
   }
 }
--- H_Lee_MyFavouriteMovies/src/app/models/list.ts
+++ H_Lee_MyFavouriteMovies/src/app/models/list.ts
@@ -1,9 +1,9 @@
 import {Content} from "./content";
 
 export class List {
-
   private _items: Content[];
-  constructor(item: Content) {
+
+  constructor() {
     this._items = []; // initialize Content array to be empty
   }
 
@@ -11,16 +11,32 @@ export class List {
     return this._items;
   }
 
-  addContent(Content: any) {
-    this._items.push(Content);
+  //addContent(Content: any)
+
+  addContent(oneitem: Content): void {
+    this._items.push(oneitem);
   }
 
-  contentLength(){
+  getLength(): number {
     return this._items.length;
   }
 
+  printProperties(index: number): string {
+    return `<div class="content-wrapper" style="text-align: center; width: 500px; margin: 0 auto;">
+              <h1>${this._items[index].title}</h1>
+              <p>${this._items[index].body}</p>
+              <h6 style="color: dodgerblue;">${this._items[index].author}</h6>
+              <img src="${this._items[index].imageLink}">
+              <div style="font-weight: bold;">${this._items[index].type}</div>
+            </div>`;
+    if (this._items.length <= index || index < 0) {
+      return `<h2>Error, invalid index requested: ${index}</h2>`;
+    }
+  }
+
+
   readContent(item: number){
-    if(item <= this.contentLength()){
+    if(item <= this.getLength()){
         return `<h3 style="color: coral">title</h3>
                 <p>something</p>
                 <a>image</a>`;
