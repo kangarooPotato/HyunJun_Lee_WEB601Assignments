2e92c3f38e518817eb47b57c0b608b22c1313900
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-05-24 23:47:52 -0400

    Content Card CSS

--- H_Lee_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ H_Lee_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {Content} from "../models/content";
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
+
 export class ContentListComponent implements OnInit {
+
+
 
   constructor() { }
 
