6a21e4b161b4d55278f65519b6fa1681f678da76
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-07-19 13:38:04 -0400

    generate movie type pipe

--- H_Lee_MyFavouriteMovies/src/app/app.module.ts
+++ H_Lee_MyFavouriteMovies/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { FilterMovieTypePipe } from './filter-movie-type.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    FilterMovieTypePipe
   ],
   imports: [
     BrowserModule
---
+++ H_Lee_MyFavouriteMovies/src/app/filter-movie-type.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterMovieTypePipe } from './filter-movie-type.pipe';
+
+describe('FilterMovieTypePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterMovieTypePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
