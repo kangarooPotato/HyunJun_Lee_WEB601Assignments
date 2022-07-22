5a072ed79496afeb6e923692aefb7ab76bd9fb20
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-07-22 13:45:49 -0400

    2

---
+++ H_Lee_MyFavouriteMovies/src/app/services/in-memory-data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { MOVIES } from '../data/mock-MOVIE';
+import { Content } from '../models/content';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+
+  createDb() {
+    // setting it to the value of our array of content
+    // 데이터 베이스 만드는듯
+    // 그리고 그 어레이 오브 데이터(content)를 리턴해줌
+    const content: Content[] = MOVIES;
+    return { content };
+  }
+
+  // id 가져오기 인데 
+  // 시뮬레이션된 데이터베이스에 새 콘텐츠를 추가할때 새 콘텐츠의 아이디를 설정해야하는데 그때 돌림
+  genId(content: Content[]): number {
+
+    // ( ? <- 조건이 참인경우 왼쪽꺼 실행, 아닌경우 : 다음꺼 실행)
+    // 콘텐츠 랭쓰(영화 데이터)가 하나라도 있으면(over 0) 콘텐츠 아이디에 +1 줌
+    // 그게 아니라면 0으로 설정 ( : <- 이게 그 역할임 ) ( || <- 는 둘 중 하나만 트루면 트루를 리턴함)
+    return content.length > 0 ? Math.max(...content.map(c => c.id || 0)) + 1 : 0;
+  }
+  // constructor() { }
+}
+  
