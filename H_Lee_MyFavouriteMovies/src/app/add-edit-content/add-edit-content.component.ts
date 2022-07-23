f9492582c43a3fa334e2a36a3ef798b822a3ca95
kangarooPotato <90293400+kangarooPotato@users.noreply.github.com>
2022-07-22 23:49:17 -0400

    Assignment 7 failed

--- H_Lee_MyFavouriteMovies/src/app/add-edit-content/add-edit-content.component.ts
+++ H_Lee_MyFavouriteMovies/src/app/add-edit-content/add-edit-content.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Content } from '../models/content';
 import { MovieService } from '../services/movie.service';
+import { MOVIES } from '../data/mock-MOVIE';
 
 @Component({
   selector: 'app-add-edit-content',
@@ -9,6 +10,17 @@ import { MovieService } from '../services/movie.service';
   styleUrls: ['./add-edit-content.component.scss']
 })
 export class AddEditContentComponent implements OnInit {
+
+  getLength(i: number) {
+    // setting it to the value of our array of content
+    // 데이터 베이스 만드는듯
+    // 그리고 그 어레이 오브 데이터(content)를 리턴해줌
+    const movies: Content[] = MOVIES;
+
+    console.log(MOVIES.length + 1);
+
+    return MOVIES.length + i;
+  }
 
   newContent: Content;
   
@@ -21,15 +33,23 @@ export class AddEditContentComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+  //   this.route.paramMap.subscribe((params) => {
+  //     this.id = Number(params.get("id") ?? 0);
+
+  //     this.movieService.getContentItem(this.id).subscribe((movieCard) => {
+  //       this.movieCard = movieCard;
+  //     });
+  // });
+
     this.route.paramMap.subscribe((params) => {
-      let id = Number(params.get("id") ?? -1);
+      let id = Number(params.get("id") ?? this.getLength(1));
       
       this.newContent.id = id;
-
       if (this.newContent.id !== -1) {
         this.movieService.getContentItem(this.newContent.id)
-          .subscribe((movieServiceToBeUpdated) => {
-            this.newContent = movieServiceToBeUpdated;
+          .subscribe((movieToBeUpdated) => {
+            this.newContent = movieToBeUpdated;
           });
       }
     });
@@ -40,7 +60,8 @@ export class AddEditContentComponent implements OnInit {
     this.movieService.inputContentItem(this.newContent).subscribe(newItem => {
       this.newContent = {
         title: '',
-        author: ''
+        author: '',
+        type:'',
       }
       console.log("The content the server gave me back: ", newItem);
     })
@@ -50,6 +71,7 @@ export class AddEditContentComponent implements OnInit {
     console.log("Content before I sent the server: ", this.newContent);
     this.movieService.updateContentItem(this.newContent).subscribe(() => {
       this.newContent = {
+        // id: this.newContent.id,
         title: '',
         author: ''
       }
